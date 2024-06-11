fa44cd8dc5e36926d6917d8324cc6ff60bc5c52b
Ashan Rajapaksha <ashanrajapaksha954@gmail.com>
2024-06-11 08:03:27 +0530

    feat: Change nav click funtionality

--- src/shared/nav/Nav.jsx
+++ src/shared/nav/Nav.jsx
@@ -45,44 +45,32 @@ function Nav() {
   }, [links]);
 
   const handleLinkClick = (index) => {
-    setActiveNavItem(index);
-
-    // Scroll to the clicked section
-    scroller.scrollTo(links[index].link, {
-      smooth: true,
-      offset: -100, // Adjust the offset as needed
-      duration: 500,
-    });
+    // setActiveNavItem(index);
+    // scroller.scrollTo(links[index].link, {
+    //   smooth: true,
+    //   offset: -100,
+    //   duration: 500,
+    // });
   };
 
   return (
-    <div className="w-full fixed top-0 left-0">
+    <div className="w-full fixed top-0 left-0 z-100 bg-blue-100">
       <div className="md:px-10 md:py-4 py-8 px-7 md:flex justify-center items-center bg-transparent">
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className="w-7 h-7 absolute left-8 top-6 cursor-pointer md:hidden"
-        >
-          {isOpen ? <XMarkIcon /> : <Bars3BottomRightIcon />}
-        </div>
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${isOpen ? "top-12" : "top-[-490px]"}`}
         >
           {" "}
           {links.map((link, index) => (
-            <li key={index} className="my-7 md:my-0 md:ml-8 cursor-pointer">
-              <ScrollLink
-                activeClass="active"
-                to={link.link}
-                spy={true}
-                smooth={true}
-                hashSpy={true}
-                offset={-100}
-                duration={500}
+            <li
+              key={index}
+              className="md:ml-8"
+              onClick={() => handleLinkClick(index)}
+            >
+              <span
                 className={`hover:text-black relative ${activeNavItem === index ? "border-b-2 border-red-500" : ""}`}
-                onClick={() => handleLinkClick(index)}
               >
                 {link.name}
-              </ScrollLink>
+              </span>
             </li>
           ))}
         </ul>
